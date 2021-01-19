6de7c1ea10c7a9a7ea94e102306380f123a78587
adeschene <alecdeschene@gmail.com>
2021-01-18 20:09:31 -0800

    Added book dependency, validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,4 @@
 class Author < ApplicationRecord
-  has_many :books
+  has_many :books, dependent: :destroy
+  validates :name, :birth_year, presence: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
   belongs_to :author
+  validates :title, :published_year, presence: true
 end
